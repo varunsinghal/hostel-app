da883d15487e00c6c2e0a39a912ee388a3f0c755
varun <varunsinghal15@gmail.com>
2018-08-08 03:03:50 +0530

    Using gunicorn to run app

--- app.py
+++ app.py
@@ -15,18 +15,16 @@ def app_index():
 
 @app.before_request
 def before_request():
-    if 'username' not in session and '/public/' not in request.url:
+    if 'username' not in session and not request.endpoint.startswith('public'):
         return redirect(url_for('public.index'))
 
 
-if __name__ == '__main__':
-    from views.home import home
-    from views.public import public
-    from views.user import user
-    from views.application import application
+from views.application import application
+from views.home import home
+from views.public import public
+from views.user import user
 
-    app.register_blueprint(public, url_prefix='/public')
-    app.register_blueprint(home, url_prefix='/home')
-    app.register_blueprint(user, url_prefix='/user')
-    app.register_blueprint(application, url_prefix='/application')
-    app.run()
+app.register_blueprint(public, url_prefix='/public')
+app.register_blueprint(home, url_prefix='/home')
+app.register_blueprint(user, url_prefix='/user')
+app.register_blueprint(application, url_prefix='/application')
